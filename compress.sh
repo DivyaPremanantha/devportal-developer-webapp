d91c734187f9a77ab0a085f880a03cda97ded09d
Divya Premanantha <premananthadivya@gmail.com>
2024-08-12 11:34:07 +0530

    Modify compress logic

--- compress.sh
+++ compress.sh
@@ -1,9 +1,2 @@
-mkdir temp_dir
-cp -r public/images temp_dir/
-cp -r public/styles temp_dir/
-cp -r src/content temp_dir/
-cp -r src/views temp_dir/
-cd temp_dir
-zip -r ../$1.zip .
-cd ..
-rm -rf temp_dir
+cd src/
+zip -r ABCOrganization.zip images layout pages partials
